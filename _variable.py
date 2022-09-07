0e5e7df4a18460e1ed38cf6c507f61d04f0b66db
Aantima005 <112957911+Aantima005@users.noreply.github.com>
2022-09-07 07:49:39 +0530

    my first program

---
+++ _variable.py
@@ -0,0 +1,26 @@
+num = 20
+num1= 30
+#num is here variable 
+#2 is value
+print(num1)
+print(num)
+# "" only use for massage
+#you should not use "" for variable
+print("Age:",num)
+print("Age: ",num1)
+#rules to define identifier
+#it should start only with "a t0 z","A to Z", or underscore_
+#no more space allowed
+# case sensitive
+# no special charecter are allowed except underscore
+# Dollar sign $ too not allowed
+# it only allowed 'a to z','A to Z' and '0 to 9'
+# No keywords allowed like 'for loop','while loop'....
+# English word which already have used in python
+# In python we ahve used 33 keywords
+# *cd = change derectory
+# cls = clear
+# 30 keywords are in small letter and only 3 keywords start with capital letter that 'True','False','None'
+# Good practics
+# Identifier should always be define a small letter
+
